4e37fcc167d7a4192f4173e57b24b33239f42071
nrmalone <nicholasrmalone99@gmail.com>
2022-02-09 19:56:09 -0800

    added dictionary

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print("hello");
+import dictionary;
+from tkinter import *;
+import json;
+
+root = Tk();
+root.title("Vocablemy test")
+
+dictionary = json.loads(dictionary.dictionary)
+
+word = "testing"
+
+def isValid(word):
+    isInDictionary = "no"
+    if (word in dictionary):
+         isInDictionary = "yes"
+    return isInDictionary
+
+testLabel = Label(root, text=isValid(word))
+testLabel.grid(row=0,column=0)
+
+root.mainloop()
